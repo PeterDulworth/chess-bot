42a6e39a826f94fb7519c6028035ed6a155b2a92
PeterDulworth <pdulworth97@gmail.com>
2024-05-08 02:53:43 -0700

    add game phase detecting and finish testing

--- engines/minimax_bot/game_phase.py
+++ engines/minimax_bot/game_phase.py
@@ -1,27 +1,73 @@
 """
-Detect which phase of the game we are in: Beginning, middle, end.
+Detect which phase of the game we are in: opening, middle, end.
 """
 
 from enum import StrEnum
+import chess
 
 
 class GamePhase(StrEnum):
-    EARLY = "early"
+    OPENING = "opening"
     MIDDLE = "middle"
     END = "end"
 
 
-def get_game_phase():
-    pass
+def get_num_major_and_minor_pieces(board: chess.Board):
+    bishops = len(board.pieces(chess.BISHOP, chess.WHITE)) + len(board.pieces(chess.BISHOP, chess.BLACK))
+    knights = len(board.pieces(chess.KNIGHT, chess.WHITE)) + len(board.pieces(chess.KNIGHT, chess.BLACK))
+    rooks = len(board.pieces(chess.ROOK, chess.WHITE)) + len(board.pieces(chess.ROOK, chess.BLACK))
+    queens = len(board.pieces(chess.QUEEN, chess.WHITE)) + len(board.pieces(chess.QUEEN, chess.BLACK))
+    return bishops + knights + rooks + queens
 
 
-def is_early_game():
-    return get_game_phase() == GamePhase.EARLY
+def is_either_back_rank_sparse(board: chess.Board):
+    """
+    We say the back-rank is sparse if the "owning" color, has less than 4 pieces on it.
+    e.g. for white: if the 1st rank has less than 4 white pieces, then the back-rank is sparse.
+
+    This function returns true if EITHER back rank is sparse.
+    """
+    # list of pieces on each back rank
+    white_back_rank = board.piece_map(mask=chess.BB_RANKS[0]).values()
+    black_back_rank = board.piece_map(mask=chess.BB_RANKS[7]).values()
+
+    # list of pieces of corresponding color on back rank
+    white_pieces_on_white_back_rank = list(filter(lambda p: p.color == chess.WHITE, white_back_rank))
+    black_pieces_on_black_back_rank = list(filter(lambda p: p.color == chess.BLACK, black_back_rank))
+
+    return len(white_pieces_on_white_back_rank) < 4 or len(black_pieces_on_black_back_rank) < 4
 
 
-def is_middle_game():
-    return get_game_phase() == GamePhase.MIDDLE
+def get_game_phase(board: chess.Board):
+    """
+    Opening: starts on move 1.
+
+    Mid-game (any of the following):
+        - There are less than 11 major + minor pieces on the board
+        - There are less than 4 pieces of the corresponding color on the back rank for either color
+
+    End-game:
+        - when there are less than 7 minor and major pieces on the board the
+          end-game has begun (so, perhaps 2 rooks and a bishop Vs 2 bishops and a knight).
+    """
+
+    num_major_and_minor_pieces = get_num_major_and_minor_pieces(board)
+
+    if num_major_and_minor_pieces < 7:
+        return GamePhase.END
+    elif num_major_and_minor_pieces < 11 or is_either_back_rank_sparse(board):
+        return GamePhase.MIDDLE
+    else:
+        return GamePhase.OPENING
 
 
-def is_end_game():
-    return get_game_phase() == GamePhase.END
+def is_opening_game(board: chess.Board):
+    return get_game_phase(board) == GamePhase.OPENING
+
+
+def is_middle_game(board: chess.Board):
+    return get_game_phase(board) == GamePhase.MIDDLE
+
+
+def is_end_game(board: chess.Board):
+    return get_game_phase(board) == GamePhase.END
--- engines/minimax_bot/test/test_game_phase.py
+++ engines/minimax_bot/test/test_game_phase.py
@@ -1,13 +1,17 @@
 import chess
 import unittest
 
-from engines.minimax_bot.game_phase import get_num_major_and_minor_pieces
+from engines.minimax_bot.game_phase import get_num_major_and_minor_pieces, is_either_back_rank_sparse, get_game_phase, \
+    GamePhase, is_middle_game, is_end_game, is_opening_game
+
+starting_position: chess.Board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+kings_only: chess.Board = chess.Board("3k4/8/8/8/8/8/8/3K4 w - - 0 1")
 
 
 class TestGamePhase(unittest.TestCase):
     def test_get_num_minor_and_major_pieces(self):
         # Given the starting board
-        board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        board = starting_position
 
         # When
         num_minor_and_major_pieces_1 = get_num_major_and_minor_pieces(board)
@@ -16,7 +20,7 @@ class TestGamePhase(unittest.TestCase):
         self.assertEqual(num_minor_and_major_pieces_1, 14)
 
         # Given kings only
-        board = chess.Board("3k4/8/8/8/8/8/8/3K4 w - - 0 1")
+        board = kings_only
 
         # When
         num_minor_and_major_pieces_2 = get_num_major_and_minor_pieces(board)
@@ -33,3 +37,64 @@ class TestGamePhase(unittest.TestCase):
         # Then
         self.assertEqual(num_minor_and_major_pieces_2, 2)
 
+    def test_is_either_back_rank_sparse(self):
+        # Given
+        board = starting_position
+        # When
+        is_sparse = is_either_back_rank_sparse(board)
+        # Then
+        self.assertEqual(is_sparse, False)
+
+        # Given
+        board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 0 1")
+        # When
+        is_sparse = is_either_back_rank_sparse(board)
+        # Then
+        self.assertEqual(is_sparse, True)
+
+        # Given
+        board = chess.Board("rn2k3/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQq - 0 1")
+        # When
+        is_sparse = is_either_back_rank_sparse(board)
+        # Then
+        self.assertEqual(is_sparse, True)
+
+        # Given
+        board = chess.Board("rn2k3/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQq - 0 1")
+        # When
+        is_sparse = is_either_back_rank_sparse(board)
+        # Then
+        self.assertEqual(is_sparse, True)
+
+    def test_get_game_phase(self):
+        # Given
+        board = starting_position
+        # When
+        game_phase = get_game_phase(board)
+        # Then
+        self.assertEqual(game_phase, GamePhase.OPENING)
+        self.assertTrue(is_opening_game(board))
+
+        # Given kings only
+        board = kings_only
+        # When
+        game_phase = get_game_phase(board)
+        # Then
+        self.assertEqual(game_phase, GamePhase.END)
+        self.assertTrue(is_end_game(board))
+
+        # Given 2 pairs of minor pieces exchanged
+        board = chess.Board("r1bqk2r/p1p2ppp/2p1pn2/3pB3/3PP3/8/PPP2PPP/RN1QK2R b KQkq - 0 8")
+        # When
+        game_phase = get_game_phase(board)
+        # Then
+        self.assertEqual(game_phase, GamePhase.MIDDLE)
+        self.assertTrue(is_middle_game(board))
+
+        # Given white back rank sparse
+        board = chess.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 0 1")
+        # When
+        game_phase = get_game_phase(board)
+        # Then
+        self.assertEqual(game_phase, GamePhase.MIDDLE)
+        self.assertTrue(is_middle_game(board))
